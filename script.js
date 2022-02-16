13eb99e3193f84ebb12ae0f6b6b4d001b5906f48
avagagner <46656654+avagagner@users.noreply.github.com>
2022-02-15 18:56:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,104 @@
-var mymap = L.map('mapid').setView([38.63775922635261, -90.28368046396037], 13);
-//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
-//https://wiki.openstreetmap.org/wiki/Tile_servers
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(mymap);
 
-var marker = L.marker([38.63672876590212, -90.24674730564345]).addTo(mymap);
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
 
-var marker2 = L.marker([38.63816176328897, -90.25104626536422]).addTo(mymap);
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
+      });
+      
+      var camera = new Camera({
+            position: [-71.05400740304519,
+  42.36596919270093,
+          450// elevation in meters
+        ],
+        tilt:60,
+        heading: 0
+      })
+      
+      var camera3 = new Camera({
+            position: [-71.1167,
+  42.3770,
+          450// elevation in meters
+        ],
+        tilt:60,
+        heading: 0
+      })
+      
+      var camera2 = new Camera({
+  position: [
+							 -71.04444248967123,
+							42.35123081816361,
+							500// elevation in meters
+						],
+        tilt: 60,
+        heading: 0
+      });
 
-var marker3 = L.marker([38.661539699331904, -90.30894447950988]).addTo(mymap);
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        camera: camera3,
+        viewingMode:"global",
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
+     
 
-var marker4 = L.marker([38.61714841986633, -90.27511168787689]).addTo(mymap);
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [stl, bei,v3].forEach(function(button) {
+      button.style.display = 'flex';
+      view.ui.add(button, 'top-right');
+    });
+    
+    bei.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera2
+      });
+    });
+     v3.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: -70.9628559,
+          y: 42.33616242, 
+          z: 500
+        },
+        tilt: 75,
+        heading: 285
+      });
+    });
+    stl.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera
+      });
+    });
 
-var marker5 = L.marker([38.64751484426265, -90.2744901289432]).addTo(mymap);
 
-
-
-marker.bindPopup("<b>Scottish Arms</b>.").openPopup();
-marker2.bindPopup("<b>Scarlett's Wine Bar</b>.").openPopup();
-marker3.bindPopup("<b>Mi Ranchito</b>.").openPopup();
-marker4.bindPopup("<b>Lorenzo's Trattoria</b>.").openPopup();
-marker4.bindPopup("<b>2Shae Cafe</b>.").openPopup();
+    });
